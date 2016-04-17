1d72fdfaad813c62499d00e5e00777a3e63e354d
jon-wade <jonwadeuk@gmail.com>
2016-04-17 15:13:29 +0100

    add in a controller and gender selector

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+
+angular.module('myApp', [])
+    .controller('myCtrl', function($scope){
+        $scope.Name='';
+        $scope.tediousTask='';
+        $scope.dirtyTask='';
+        $scope.celebrity='';
+        $scope.uselessTask='';
+        $scope.obnoxiousCelebrity='';
+        $scope.hugeNumber='';
+        $scope.adjective='';
+        $scope.jobTitle='';
+        $scope.gender='male';
+    });
+
