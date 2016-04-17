7685ea37edb7ebab1461f574cb470a377d6153f1
jon-wade <jonwadeuk@gmail.com>
2016-04-17 19:09:40 +0100

    refactor to include error detection and show-hide functionality

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 
 angular.module('myApp', [])
     .controller('myCtrl', function($scope){
-        $scope.Name='';
+        $scope.name='';
         $scope.tediousTask='';
         $scope.dirtyTask='';
         $scope.celebrity='';
@@ -11,5 +11,29 @@ angular.module('myApp', [])
         $scope.adjective='';
         $scope.jobTitle='';
         $scope.gender='male';
+
+        $scope.submit = function(){
+            console.log($scope.inputForm);
+            if($scope.inputForm.$invalid){
+                $scope.error=true;
+            }
+            else {
+                $scope.show=true;
+            }
+        }
+
+        $scope.startOver = function(){
+            $scope.show=false;
+            $scope.name='';
+            $scope.tediousTask='';
+            $scope.dirtyTask='';
+            $scope.celebrity='';
+            $scope.uselessTask='';
+            $scope.obnoxiousCelebrity='';
+            $scope.hugeNumber='';
+            $scope.adjective='';
+            $scope.jobTitle='';
+        }
+
     });
 
